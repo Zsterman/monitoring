d6e20d0e0f3ed95f44ab091b27986e2a0b2b78df
Zachary Sterman <zacharysterman1992@gmail.com>
2021-10-26 14:18:38 -0400

    update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res)=> {
 
 let students = []
 
-app.post('/api/students', (req, res) => {
+app.post('/api/student', (req, res) => {
     let {name} = req.body
     name = name.trim()
 
