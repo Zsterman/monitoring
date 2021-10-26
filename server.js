4f731445944f7d59b03d596aa294fd2b40d500a9
Zachary Sterman <zacharysterman1992@gmail.com>
2021-10-26 14:02:57 -0400

    added handler

--- server.js
+++ server.js
@@ -19,7 +19,19 @@ app.get('/', (req, res)=> {
     rollbar.info('html file served successfully')
 })
 
+let students = []
 
+app.post('/api/students', (req, res) => {
+    const {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+    rollbar.log('Student added successfully', {author: 'Zachary', type: 'manual entry'})
+    res.status(200).send(students)
+
+})
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4545
 
